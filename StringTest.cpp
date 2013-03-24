a80744d6e58e1fdeca4883d860ec3e055cbf0aaf
28kayak <kaya_ota@msn.com>
2013-03-23 23:38:19 -0700

    inital commit

---
+++ StringTest.cpp
@@ -0,0 +1,17 @@
+//string::operator[]
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+ 	string str ("Test String");
+	//using string constractor, defined string str is array of char.
+	//string type name also indeicas array name.  
+	for(int i = 0; i < str.length(); ++i)
+	{
+		cout << str[i] << "\n";
+	}
+	return 0;
+}
