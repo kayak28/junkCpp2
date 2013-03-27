aec627ef5a9018b594cbd1e22548bbf224382721
28kayak <kaya_ota@msn.com>
2013-03-27 01:31:51 -0700

    edit

--- AtoiTest.cpp
+++ AtoiTest.cpp
@@ -1,5 +1,5 @@
 //atoi function sample 
-
+#include <string>
 #include "stdio.h"//where print functions are stored 
 #include "stdlib.h"//where atoi is stored 
 #include <iostream>
@@ -7,14 +7,18 @@ using namespace std;
 
 int main()
 {
+	string str("this is kaya");
 	int i;
+	int ans;
 	char buffer[256]; // 256 indexes of an array named buffer
 	for(int m = 0; m < 3; m++)
 	{
 		printf("Enter a number: ");
 		fgets(buffer, 256, stdin); 
+		ans = atoi(str[m]);
 		i = atoi(buffer);
-		cout << i << "\n";
+			
+		cout << m << "\n";
 		printf("the value entered is %d", i);
 	} 
 	return 0;
